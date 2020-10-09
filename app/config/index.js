568ba2ad29e129f7c9fd784dd55901fe2ada6fa2
TienTruong <nguyenthientruong1998@gmail.com>
2020-10-09 20:26:57 +0700

    pretier

--- app/config/index.js
+++ app/config/index.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 async function connect() {
-  try {
-    await mongoose.connect("mongodb://localhost:27018`/f8_education_dev", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
-    console.log("Connect oke");
-  } catch (error) {
-    console.log("Connect fails : " + error);
-  }
+    try {
+        await mongoose.connect('mongodb://localhost:27018`/f8_education_dev', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        });
+        console.log('Connect oke');
+    } catch (error) {
+        console.log('Connect fails : ' + error);
+    }
 }
 
 module.exports = { connect };
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const courseRoute = require('./CourseRoute');
 const meRoute = require('./MeRoute');
 
 function routes(app) {
-    
     app.use('/me', meRoute);
     app.use('/news', newsRoute);
     app.use('/course', courseRoute);
