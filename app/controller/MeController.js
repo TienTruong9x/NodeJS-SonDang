b2c2f15990f182f659d242a5dd821311c839f24c
TienTruong9x <59884685+TienTruong9x@users.noreply.github.com>
2020-10-13 10:52:58 +0700

    Pretier

--- app/controller/MeController.js
+++ app/controller/MeController.js
@@ -3,13 +3,11 @@ const Course = require('../models/Course');
 class MeController {
     //[GET] /me/course
     courseMe(req, res, next) {
-        res.render("me/meCourse");
+        res.render('me/meCourse');
     }
 
     //[GET]  /me/news
-    courseNews(req, res,next) {
-        
-    }
+    courseNews(req, res, next) {}
 }
 
 module.exports = new MeController();
